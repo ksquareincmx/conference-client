cef489073b1cd1abd282aa301895c2102bf73502
basultobdKs <daniel.basulto@ksquareinc.com>
2019-03-06 17:25:00 -0600

    add dirs and index files for each service

--- src/services/index.js
+++ src/services/index.js
@@ -1,9 +1,9 @@
-import { AuthService } from "./AuthService";
-import { StorageService } from "./StorageService";
-import { BookingService } from "./BookingService";
-import { ProfileService } from "./ProfileService";
-import { RoomService } from "./RoomService";
-import { UserService } from "./UserService";
+import { AuthService } from "./Auth";
+import { StorageService } from "./Storage";
+import { BookingService } from "./Booking";
+import { ProfileService } from "./Profile";
+import { RoomService } from "./Room";
+import { UserService } from "./User";
 
 // TODO: Pass the storage service to the services
 const authService = AuthService();
