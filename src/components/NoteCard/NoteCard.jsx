90c58cce9504a1f8779de62b0eb3c448f6ba8194
Javier <javier.mijangos@ksquareinc.com>
2019-04-19 18:06:00 -0500

    fix (Calendar): add handling of the calendar when there are no rooms on the data base

---
+++ src/components/NoteCard/NoteCard.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { withStyles } from "@material-ui/core/styles";
+import { Paper, Typography } from "@material-ui/core";
+
+const styles = theme => ({
+  container: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "calc(100vh - 84px)",
+    width: "100%"
+  },
+  note: {
+    width: "20%",
+    padding: 20,
+    backgroundColor: "red"
+  },
+  txt: {
+    color: "white"
+  }
+});
+
+function NoteCardComponent(props) {
+  const { container, note, txt } = props.classes;
+
+  return (
+    <div className={container}>
+      <Paper className={note} elevation={1}>
+        <Typography className={txt} variant="h5" component="h3">
+          Rooms not found
+        </Typography>
+        <Typography className={txt} component="p">
+          The data base of the application needs to have rooms registered to
+          work.
+        </Typography>
+      </Paper>
+    </div>
+  );
+}
+
+export const NoteCard = withStyles(styles)(NoteCardComponent);
