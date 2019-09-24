316f9c6ae48b5f4e7e07523953fea8ae4c798c23
Michell Ayala <starships@outlook.com>
2019-09-23 21:11:48 -0500

    clean(BookingItem): replace inline styles by styled-components

--- src/components/BookingsSideBar/BookingItem.tsx
+++ src/components/BookingsSideBar/BookingItem.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import * as colors from "styles/colors";
+import styled from "styled-components";
 
 const toHumanHour = (date: string): string => {
   const d = new Date(date);
@@ -10,47 +11,56 @@ const toHumanHour = (date: string): string => {
   return `${hours}:${minutes}`;
 };
 
-export const BookingItem: React.FC<any> = props => {
-  const { booking } = props;
+export const BookingItemContainer = styled.div`
+  border: 1px solid ${colors.STROKE};
+  border-radius: 4px;
+  box-sizing: border-box;
+  margin-bottom: 1rem;
+  padding: 1rem;
+  color: ${colors.TEXT};
+`;
 
+export const BookingItemDesc = styled.h3`
+  font-size: 1.2rem;
+  font-weight: bold;
+  margin: 0;
+`;
+
+export interface IBookingItemColorProps {
+  color: string;
+}
+
+export const BookingItemColor = styled.div<IBookingItemColorProps>`
+  background-color: ${props => props.color};
+  border-radius: 100%;
+  display: inline-block;
+  height: 0.5rem;
+  vertical-align: middle;
+  width: 0.5rem;
+`;
+
+export const BookingItemRoom = styled.div`
+  display: inline-block;
+  margin: 0 0 0 0.5rem;
+  vertical-align: middle;
+`;
+
+export const BookingItemTime = styled.p`
+  margin: 0;
+  margin-left: 1rem;
+`;
+
+export const BookingItem: React.FC<any> = ({ booking }) => {
   return (
-    <div
-      style={{
-        border: `1px solid ${colors.STROKE}`,
-        borderRadius: "4px",
-        boxSizing: "border-box",
-        marginBottom: "1rem",
-        padding: "1rem",
-        color: "#000",
-      }}
-    >
-      <h3 style={{ fontSize: "1.2rem", fontWeight: "bold", margin: "0" }}>
-        {booking.description}
-      </h3>
-      <span
-        style={{
-          backgroundColor: booking.roomColor,
-          borderRadius: "100%",
-          display: "inline-block",
-          height: "0.5rem",
-          verticalAlign: "middle",
-          width: "0.5rem",
-        }}
-      ></span>
-      <p
-        style={{
-          display: "inline-block",
-          margin: "0 0 0 0.5rem",
-          verticalAlign: "middle",
-        }}
-      >
+    <BookingItemContainer>
+      <BookingItemDesc>{booking.description}</BookingItemDesc>
+      <BookingItemColor color={booking.roomColor} />
+      <BookingItemRoom>
         Room {booking.room.id} - {booking.room.name}
-      </p>
-      <div style={{ marginLeft: "1rem" }}>
-        <p style={{ margin: 0 }}>
-          {toHumanHour(booking.start)} - {toHumanHour(booking.end)}
-        </p>
-      </div>
-    </div>
+      </BookingItemRoom>
+      <BookingItemTime>
+        {toHumanHour(booking.start)} - {toHumanHour(booking.end)}
+      </BookingItemTime>
+    </BookingItemContainer>
   );
 };
